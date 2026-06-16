7dc6767c784fd327a7ef9552c0263af64a0b20d1
jesse <jesse.frazier.8@gmail.com>
2026-06-15 23:26:58 -0400

    new basement profile for only external monitors

---
+++ profiles/basement-ext.lua
@@ -0,0 +1,12 @@
+return {
+  hl.workspace_rule({ workspace = "1", monitor = "desc:LG Electronics LG FULL HD 0x0101010" }),
+  hl.workspace_rule({ workspace = "2", monitor = "desc:LG Electronics LG FULL HD 0x0101010" }),
+  hl.workspace_rule({ workspace = "3", monitor = "desc:LG Electronics LG FULL HD 0x0101010" }),
+  hl.workspace_rule({ workspace = "4", monitor = "desc:LG Electronics LG FULL HD 0x0101010" }),
+  hl.workspace_rule({ workspace = "5", monitor = "desc:LG Electronics LG FULL HD 0x0101010" }),
+  hl.workspace_rule({ workspace = "6", monitor = "desc:Acer Technologies KA242Y 72380FE9C3W0" }),
+  hl.workspace_rule({ workspace = "7", monitor = "desc:Acer Technologies KA242Y 72380FE9C3W0" }),
+  hl.workspace_rule({ workspace = "8", monitor = "desc:Acer Technologies KA242Y 72380FE9C3W0" }),
+  hl.workspace_rule({ workspace = "9", monitor = "desc:Acer Technologies KA242Y 72380FE9C3W0" }),
+  hl.workspace_rule({ workspace = "10", monitor = "desc:Acer Technologies KA242Y 72380FE9C3W0" }),
+}
